1b0f16d0b740a6eeada444218e7e29367dd74174
Dmitry Parkhomenko <oku_74@mail.ru>
2023-11-17 20:43:47 +0500

    Initial Commit

---
+++ Homework_2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.4
+//
+//  Created by Dmitry Parhomenko on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
